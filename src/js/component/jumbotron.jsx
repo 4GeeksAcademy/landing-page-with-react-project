d640dbf09ba315121b36389f4f60b5d442d54e0e
María Ripoll Vives <mariaripoll95@icloud.com>
2023-08-12 17:43:32 +0000

    Created the Jumbotron and importeed it to the Home file

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,18 +3,12 @@ import React from "react";
 //create your first component
 const Jumbotron = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+		<div className="jumbotron">
+			<h1 className="display-4">Hello, world!</h1>
+			<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+			<p>It uses utility for typography and spacing to space content out within the larger container.</p>
+			<p className="lead">
+				<a className="btn btn-dark btn-lg" href="#" role="button">Learn more</a>
 			</p>
 		</div>
 	);
